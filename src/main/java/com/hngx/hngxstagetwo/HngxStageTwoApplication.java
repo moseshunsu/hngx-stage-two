ae80c6d39d50145d001517adfc4f9168aa7123c6
Moses Hunsu <hunsumoses7@gmail.com>
2023-09-14 15:54:34 +0100

    Initial commit

---
+++ src/main/java/com/hngx/hngxstagetwo/HngxStageTwoApplication.java
@@ -0,0 +1,13 @@
+package com.hngx.hngxstagetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HngxStageTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HngxStageTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hngx/hngxstagetwo/HngxStageTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hngx.hngxstagetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HngxStageTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
